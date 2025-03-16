9561969781f55a7b5931ace8dff5143c7a707cb8
Fauzan Azima <fauzannazimaaa123@gmail.com>
2025-03-17 01:03:49 +0700

    memperbaiki tampilan login & regis

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
 
                                 <div class="col-12">
                                     <label for="password"
-                                        class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                        class="form-label">{{ __('Password') }}</label>
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="current-password">
