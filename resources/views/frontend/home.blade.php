e524b10ad79520aeac7e4f64e1cd2b4b0b31edeb
Fauzan Azima <fauzannazimaaa123@gmail.com>
2025-03-19 21:33:51 +0700

    memperbaiki blade  templating

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,180 @@
+@extends ('frontend.template.main')
+
+@section('content')
+
+    <!-- Start Hero Section -->
+    <div class="hero">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-5">
+                    <div class="intro-excerpt">
+                        <h1>Selamat Datang <span clsas="d-block">Di Studio Kami</span></h1>
+                        <p class="mb-4">Studio kami siap memenuhi segala kebutuhan anda! Dari desain kreatif hingga proses
+                            cetak berkualitas tinggi, semuanya ditangani oleh tenaga profesional dan berpengalaman untuk
+                            memastikan hasil terbaik bagi anda.</p>
+                        <p><a href="{{ route('frontend.shop') }}" class="btn btn-secondary me-2">Shop Now</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="hero-img-wrap">
+                        <img src="{{ asset('frontend') }}/images/image12.png" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Hero Section -->
+
+    <!-- Start Product Section -->
+    <div class="product-section">
+        <div class="container">
+            <div class="row">
+
+                <!-- Start Column 1 -->
+                <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+                    <h2 class="mb-4 section-title">Di Cetak Menggunakan Mesin Modern.</h2>
+                    <p class="mb-4">Studio kami sudah menggunakan mesin cetak terbaru, sehingga hasil cetak anda akan
+                        terlihat lebih berkualitas dan tahan lama. </p>
+                    <p><a href="shop.html" class="btn">Explore</a></p>
+                </div>
+                <!-- End Column 1 -->
+
+                <!-- Start Column 2 -->
+                @foreach ($produck as $produk)
+                    <div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                        <a class="product-item" href="#" data-bs-toggle="modal" data-bs-target="#imageModal{{ $produk->id }}">
+                            <img src="{{ asset($produk->image) }}" class="img-fluid product-thumbnail">
+                            <h3 class="product-title">{{ $produk->jasa_produk }}</h3>
+                            <strong class="product-price">Rp.{{ number_format($produk->harga, 0, ',', '.') }}</strong>
+                        </a>
+                    </div>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="imageModal{{ $produk->id }}" tabindex="-1"
+                        aria-labelledby="imageModalLabel{{ $produk->id }}" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-lg">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="imageModalLabel{{ $produk->id }}">{{ $produk->nama_produk }}
+                                    </h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <img src="{{ asset($produk->image) }}" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                <!-- End Column 2 -->
+
+            </div>
+        </div>
+    </div>
+    <!-- End Product Section -->
+
+    <!-- Start Popular Product -->
+    <div class="popular-product">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                    <div class="product-item-sm d-flex">
+                        <div class="thumbnail">
+                            <img src="images/product-1.png" alt="Image" class="img-fluid">
+                        </div>
+                        <div class="pt-3">
+                            <h3>Nordic Chair</h3>
+                            <p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
+                            <p><a href="#">Read More</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                    <div class="product-item-sm d-flex">
+                        <div class="thumbnail">
+                            <img src="images/product-2.png" alt="Image" class="img-fluid">
+                        </div>
+                        <div class="pt-3">
+                            <h3>Kruzo Aero Chair</h3>
+                            <p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
+                            <p><a href="#">Read More</a></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                    <div class="product-item-sm d-flex">
+                        <div class="thumbnail">
+                            <img src="images/product-3.png" alt="Image" class="img-fluid">
+                        </div>
+                        <div class="pt-3">
+                            <h3>Ergonomic Chair</h3>
+                            <p>Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio </p>
+                            <p><a href="#">Read More</a></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Popular Product -->
+
+    <!-- Start Blog Section -->
+    <div class="blog-section">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md-6">
+                    <h2 class="section-title">Recent Blog</h2>
+                </div>
+                <div class="col-md-6 text-start text-md-end">
+                    <a href="#" class="more">View All Posts</a>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
+                    <div class="post-entry">
+                        <a href="#" class="post-thumbnail"><img src="images/post-1.jpg" alt="Image" class="img-fluid"></a>
+                        <div class="post-content-entry">
+                            <h3><a href="#">First Time Home Owner Ideas</a></h3>
+                            <div class="meta">
+                                <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 19, 2021</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
+                    <div class="post-entry">
+                        <a href="#" class="post-thumbnail"><img src="images/post-2.jpg" alt="Image" class="img-fluid"></a>
+                        <div class="post-content-entry">
+                            <h3><a href="#">How To Keep Your Furniture Clean</a></h3>
+                            <div class="meta">
+                                <span>by <a href="#">Robert Fox</a></span> <span>on <a href="#">Dec 15, 2021</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
+                    <div class="post-entry">
+                        <a href="#" class="post-thumbnail"><img src="images/post-3.jpg" alt="Image" class="img-fluid"></a>
+                        <div class="post-content-entry">
+                            <h3><a href="#">Small Space Furniture Apartment Ideas</a></h3>
+                            <div class="meta">
+                                <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 12, 2021</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Blog Section -->
+
+@endsection
